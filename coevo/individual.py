fdcdf82888829aad652a6ce6b1ea58f506507121
echigot <estelle.chigot@etu.enseeiht.fr>
2021-06-30 17:29:25 +0200

    better fitness function

--- coevo/individual.py
+++ coevo/individual.py
@@ -2,11 +2,14 @@ import gym
 import griddly
 
 class Individual:
+    nb_steps_max = 300
+
     def __init__(self, genes):
         self.genes = genes
         self.fitness = 0
         self.done = False
         self.reward = 0
+        self.steps = 0
 
     def do_action(self, result, env):
         obs_2, reward, done, info  = env.step(result)
@@ -17,12 +20,13 @@ class Individual:
           
     def play_one_game(self, agent, env):
         cpt = 0
-        while(not self.done and cpt < 300):
+        while(not self.done or self.steps < Individual.nb_steps_max):
             result = agent.get_result(env) #find how to get a result with nn
             self.do_action(result, env)
-            cpt = cpt + 1
+            self.steps = self.steps + 1
     
 
 def fitness(agent, env):
-    return agent.reward*10 if agent.done else agent.reward
+    #add distance to ending point maybe
+    return (Individual.nb_steps_max - agent.step) if agent.done else agent.reward
     
